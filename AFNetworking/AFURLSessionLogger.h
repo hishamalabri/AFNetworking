c64d470b1fdf98671edbdb0ac9a7ad162ac763a7
Hisham Alabri <hisham.alabri@intelligentpathways.com.au>
2019-05-15 12:47:12 +1000

    Adding request/response logging to AFNetworking

---
+++ AFNetworking/AFURLSessionLogger.h
@@ -0,0 +1,17 @@
+//
+//  AFURLSessionLogger.h
+//  AFNetworking
+//
+//  Created by Hisham Alabri on 15/5/19.
+//  Copyright © 2019 AFNetworking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AFURLSessionLogger : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AFNetworking/AFURLSessionLogger.m
@@ -0,0 +1,13 @@
+//
+//  AFURLSessionLogger.m
+//  AFNetworking
+//
+//  Created by Hisham Alabri on 15/5/19.
+//  Copyright © 2019 AFNetworking. All rights reserved.
+//
+
+#import "AFURLSessionLogger.h"
+
+@implementation AFURLSessionLogger
+
+@end
